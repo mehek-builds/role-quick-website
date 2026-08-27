396835dc19dcc7b63ba36ddb2ac7b52404f69d75
Mehek Mandal <mehekmandal05@gmail.com>
2026-08-27 19:16:47 +0400

    fix: stabilize country option labels

--- lib/work-eligibility.ts
+++ lib/work-eligibility.ts
@@ -13,9 +13,15 @@ export type CountryWorkEligibilityDraft = Omit<
 export const ISO_COUNTRY_CODES = (`AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW`).split(" ");
 const ISO_COUNTRY_CODE_SET = new Set(ISO_COUNTRY_CODES);
 const countryNames = new Intl.DisplayNames(["en"], { type: "region" });
+const STABLE_REGION_NAMES: Readonly<Record<string, string>> = {
+  FK: "Falkland Islands",
+  HK: "Hong Kong SAR China",
+  MO: "Macao SAR China",
+  PS: "Palestinian Territories",
+};
 
 export const COUNTRY_OPTIONS: readonly (readonly [string, string])[] = ISO_COUNTRY_CODES
-  .map((code) => [code, countryNames.of(code) ?? code] as const)
+  .map((code) => [code, STABLE_REGION_NAMES[code] ?? countryNames.of(code) ?? code] as const)
   .sort((left, right) => left[1].localeCompare(right[1]));
 
 export function blankCountryEligibility(countryCode = ""): CountryWorkEligibilityDraft {
